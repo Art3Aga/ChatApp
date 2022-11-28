3adfbc569cf4a1499b81036f7a84c1c41d4d7aa8
Art3Aga <alberyarte@gmail.com>
2022-11-28 01:00:38 -0600

    Deploy to Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  socketUrl: 'http://localhost:8080'
+  // socketUrl: 'http://localhost:8080'
+  socketUrl: 'https://socket-server.herokuapp.com'
 };
 
 /*
